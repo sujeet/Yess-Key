0ca42c53ef3ea2b0ab1e74230551ca6ab50d3af1
Sujeet Gholap <sujeetgholap@gmail.com>
2014-01-13 18:38:04 -0800

    Draft submission. Passes all the tests. Seems faster. TODO: Documentation.       Explanation.

--- skey.js
+++ skey.js
@@ -3,20 +3,26 @@
   Assignment 1: S/KEY Authentication
   Starter Code Version: 1.0
 
-  SUNet ID #1:
-  SUNet ID #2:
+  SUNet ID #1: aparnak
+  SUNet ID #2: sujeet
 
   Step 1: Find a project partner and add your SUNet IDs above.
-  Step 2: Implement `initialize`, `advance`, `save`, and `load` in `pebble_chain`.
+  Step 2: Implement `initialize`, `advance`, `save`,
+          and `load` in `pebble_chain`.
   Step 3: Answer the questions below.
   Step 4: See Piazza for submission instructions.
 */
 
-/* 1. Briefly describe your implementation and its design choices. (e.g. What algorithm did you use? How did you structure your code? Did you do something interesting in \texttt{save}/\texttt{load}? If it's not obvious, justify the space/time used by your implementation.)
+/* 1. Briefly describe your implementation and its design choices.
+     e.g. What algorithm did you use? How did you structure your code?
+     Did you do something interesting in "save" and "load"?
+     If it's not obvious, justify the space/time used by your implementation.
 // TODO: Answer here (a few sentences).
 */
 
-/* 2. If you were designing an authentication mechanism for a hot new startup that wants to protect its users, how would you decide whether/where to use S/KEY?
+/* 2. If you were designing an authentication mechanism for
+     a hot new startup that wants to protect its users,
+     how would you decide whether/where to use S/KEY?
 // TODO: Answer here (a few sentences).
 */
 
@@ -24,7 +30,8 @@
 // TODO: Answer here (just a number).
 */
 
-/* 4. (Optional:) Do you have any comments or suggestions for improving the assignment?
+/* 4. (Optional:) Do you have any comments or suggestions
+     for improving the assignment?
 // TODO: Answer here (optional).
 */
 
@@ -35,97 +42,144 @@
 "use strict"; // Makes it easier to catch errors.
 
 var sjcl = require("./lib/sjcl");
-var hash = sjcl.hash.sha256.hash; // Hashes a string or bitArray to a bitArray.
+
+// Hashes a string or bitArray to a bitArray.
+var hash = sjcl.hash.sha256.hash;
+
 var is_equal = sjcl.bitArray.equal; // Compares two bitArrays.
 var hex = sjcl.codec.hex.fromBits; // Converts a bitArray to a hex string.
 
 var pow2 = Math.pow.bind(this, 2); // Calculates 2 to a given power.
-var log2 = function(x) {return Math.log(x) / Math.log(2);} // Calculates log base 2.
+var log2 = function(x) {return Math.log(x) / Math.log(2);};
 
+var Stack = Array;
 
 /******** Naive Hash Chain Implementation ********/
 
 
 function naive_chain() {
 
-  var chain = {
-    state: null
-  };
+    var chain = {
+        state: null
+    };
 
-  chain.initialize = function(num_iterations, seed) {
-    chain.state = {
-      position: 0,
-      num_iterations: num_iterations,
-      start: hash(seed)
-    }
+    chain.initialize = function(num_iterations, seed) {
+        chain.state = {
+            position: 0,
+            num_iterations: num_iterations,
+            start: hash(seed)
+        };
 
-    var initial = chain.state.start;
-    for (var i = 0; i < chain.state.num_iterations; i++) {
-      initial = hash(initial);
-    }
+        var initial = chain.state.start;
+        for (var i = 0; i < chain.state.num_iterations; i++) {
+            initial = hash(initial);
+        }
 
-    return initial;
-  }
+        return initial;
+    };
 
-  chain.advance = function() {
-    if (chain.state.position + 1 > chain.state.num_iterations) {
-      return null;
-    }
+    chain.advance = function() {
+        if (chain.state.position + 1 > chain.state.num_iterations) {
+            return null;
+        }
 
-    var value = chain.state.start;
-    for (var i = 1; i < chain.state.num_iterations - chain.state.position; i++) {
-      value = hash(value);
-    }
-    chain.state.position += 1;
-    return value;
-  }
+        var value = chain.state.start;
+        for (var i = 1;
+             i < chain.state.num_iterations - chain.state.position;
+             i++)
+        {
+            value = hash(value);
+        }
+        chain.state.position += 1;
+        return value;
+    };
 
-  // Returns a string.
-  chain.save = function() {
-    return JSON.stringify(chain.state);
-  }
+    // Returns a string.
+    chain.save = function() {
+        return JSON.stringify(chain.state);
+    };
 
-  // Loads a string.
-  chain.load = function(str_data) {
-    chain.state = JSON.parse(str_data);
-  }
+    // Loads a string.
+    chain.load = function(str_data) {
+        chain.state = JSON.parse(str_data);
+    };
 
-  return chain;
+    return chain;
 }
 
 
-/******** Pebble-Based Hash Chain Implementation (Jakobsson's algorithm) ********/
+/* Pebble-Based Hash Chain Implementation(Jakobsson's algorithm) */
 
+function top (arr) {
+    return arr [arr.length - 1];
+}
 
 function pebble_chain() {
 
-  var chain = {
-    state: null
-  };
+    // We start indexing the chain from 1.
+    // Chain [1] is hash (seed)
+    // Chain [2] is hash (hash (seed)) and so on...
+    var chain = {
+        state: null
+    };
+    
+    chain.initialize = function(num_iterations, seed) {
+        var num_pebbles = Math.floor (log2 (num_iterations));
+        chain.state = {
+            index : num_iterations,
+            stack : new Stack ()
+        };
 
-  chain.initialize = function(num_iterations, seed) {
-    // TODO
-    throw "pebble_chain.initialize() is not implemented yet.";
-  }
+        if (num_iterations != pow2 (num_pebbles)) {
+            chain.state.stack.push ({index : 1,
+                                     hash : hash (seed)});
+        }
 
-  chain.advance = function() {
-    // TODO
-    throw "pebble_chain.advance() is not implemented yet.";
-  }
+        var hash_value = hash (seed);
+        for (var i = 1; i <= num_iterations; i++) {
+            if (i == (num_iterations + 1 - pow2 (num_pebbles))) {
+                chain.state.stack.push ({index : i,
+                                         hash : hash_value});
+                num_pebbles -= 1;
+            }
+            hash_value = hash (hash_value);
+        }
 
-  // Returns a string.
-  chain.save = function() {
-    // TODO
-    throw "pebble_chain.save() is not implemented yet.";
-  }
+        return hash_value;
+    };
 
-  // Loads a string.
-  chain.load = function(str_data) {
-    // TODO
-    throw "pebble_chain.load() is not implemented yet.";
-  }
+    chain.advance = function() {
+        // Advance returns Chain [index]
+        // and then decreases index.
+        if (chain.state.index == 0) return null;
+        if (top (chain.state.stack).index != chain.state.index) {
+            for (var i = top (chain.state.stack).index;
+                 i < chain.state.index;
+                 i++)
+            {
+                chain.state.stack.push (
+                    {
+                        index: i + 1,
+                        hash : hash (top (chain.state.stack).hash)
+                    }
+                );
+            }
+        }
+        chain.state.index -= 1;
+        return chain.state.stack.pop().hash;
+    };
 
-  return chain;
+    // Returns a string.
+    chain.save = function() {
+        return JSON.stringify (chain.state);
+    };
+
+    // Loads a string.
+    chain.load = function(str_data) {
+        chain.state = JSON.parse (str_data);
+    };
+
+    return chain;
 }
 
 /********* Export functions for testing. ********/
